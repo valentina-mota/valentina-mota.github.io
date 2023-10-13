f3740472a2db7a581dd669795495e0891ae283a3
valentina-mota <valentina.mota@gmail.com>
2023-10-14 00:02:18 +0200

    prima versione

---
+++ main.js
@@ -0,0 +1,10 @@
+const gridItem = document.querySelectorAll(".grid__item");
+const gridItemTitle = document.getElementsByClassName("grid__item-title");
+
+
+
+gridItem.forEach(function (element) {
+    element.addEventListener("mouseover", function () {
+        gridItemTitle.class;
+    });
+});
